567bb376e2b61475170576ede04923baabaca8da
unknown <zakaariaDawoud@gmail.com>
2022-03-19 20:54:01 +0200

    hello

---
+++ Flutter/sampleproject/lib/iug.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class iug extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MY NAMES IS MOHMMED"),
+      ),
+      body: Center(
+        child: const Text("hello to iug course"),
+      ),
+    );
+  }
+}
